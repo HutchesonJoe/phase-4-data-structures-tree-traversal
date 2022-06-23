13565cc751b97b0d18d94eaf036887c1450edb5d
Joe <hutcheson2001@yahoo.com>
2022-06-23 17:46:21 -0400

    commit tree c6700d509b5b73a584a7e6165e974d1f9074d539

--- lib/tree.rb
+++ lib/tree.rb
@@ -1,5 +1,23 @@
+require "pry"
+
 class Tree
   def initialize(root = nil)
     @root = root
+  end
+
+  def get_element_by_id(string)
+   result = []
+   nodes_to_visit = [@root]
+   
+   while nodes_to_visit.length > 0
+    node = nodes_to_visit.shift
+    if node[:id] == string
+      return node
+    end
+    result.push(node[:id])
+
+    nodes_to_visit = node[:children] + nodes_to_visit
+   end
+   return
   end
 end
